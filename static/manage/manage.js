75be98cc41493f8325bd74fc0972bf2771a2e1d4
Sukunnari <151819685+sukunnari@users.noreply.github.com>
2026-09-13 21:42:38 +0800

    Add scores for room

--- static/manage/manage.js
+++ static/manage/manage.js
@@ -1,1 +1,66 @@
 // @ts-check
+const pageState = {
+	password: "",
+};
+
+document.querySelector("#form-password")?.addEventListener("submit", (e) => {
+	e.preventDefault();
+});
+
+document.querySelector("#password")?.addEventListener("change", (e) => {
+	pageState.password = e.target.value;
+});
+
+const formAddRoom = document.querySelector("#form-add-room");
+formAddRoom?.addEventListener("submit", async (e) => {
+	e.preventDefault();
+	try {
+		const fd = new FormData(e.target);
+		const res = await fetch("../api/manage/add-room", {
+			method: "POST",
+			body: JSON.stringify({
+				room: fd.get("room-id"),
+			}),
+			headers: {
+				"api-password": pageState.password,
+			},
+		});
+
+		const resData = await res.json();
+		if (resData?.success) {
+			alert("Successfully added room");
+		} else {
+			alert(resData?.message || "Server error");
+		}
+	} catch (error) {
+		alert("Error while fetching");
+		console.error(error);
+	}
+});
+
+const formAddRoomScores = document.querySelector("#form-add-room-scores");
+formAddRoomScores?.addEventListener("submit", async (e) => {
+	e.preventDefault();
+	try {
+		const fd = new FormData(e.target);
+		const res = await fetch("../api/manage/add-room-scores", {
+			method: "POST",
+			body: JSON.stringify({
+				room: fd.get("room-id"),
+			}),
+			headers: {
+				"api-password": pageState.password,
+			},
+		});
+
+		const resData = await res.json();
+		if (resData?.success) {
+			alert("Scores successfully retrieved");
+		} else {
+			alert(resData?.message || "Server error");
+		}
+	} catch (error) {
+		alert("Error while fetching");
+		console.error(error);
+	}
+});
