a33297486d432ac8e1aa8d1e4e6192b9b42d40b1
Sukunnari <151819685+sukunnari@users.noreply.github.com>
2026-09-13 15:08:12 +0800

    Wrong sqlite library whoops

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,4 +1,4 @@
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import { drizzle } from "drizzle-orm/libsql";
 import { assertString } from "../utils/assert.js";
 
 const db = drizzle(assertString(process.env.DB_URL));
