8b39119b26e8e51f7013af901eb0156803a1d76f
Sukunnari <151819685+sukunnari@users.noreply.github.com>
2026-09-13 15:38:21 +0800

    Add P Key to all tables, add player ref on scores

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -2,14 +2,14 @@ import { sqliteTable } from "drizzle-orm/sqlite-core";
 import { int, text, numeric } from "drizzle-orm/sqlite-core";
 
 export const rooms = sqliteTable("rooms", {
-	room_id: int().notNull(),
+	room_id: int().notNull().primaryKey(),
 	name: text().notNull(),
 	start_date: int({ mode: "timestamp" }).notNull(),
 	end_date: int({ mode: "timestamp" }),
 });
 
 export const playlist_items = sqliteTable("playlist_items", {
-	playlist_item_id: int().notNull(),
+	playlist_item_id: int().notNull().primaryKey(),
 	room_id: int()
 		.notNull()
 		.references(() => rooms.room_id),
@@ -22,16 +22,19 @@ export const playlist_items = sqliteTable("playlist_items", {
 });
 
 export const scores = sqliteTable("scores", {
-	score_id: int().notNull(),
+	score_id: int().notNull().primaryKey(),
 	playlist_item_id: int()
 		.notNull()
 		.references(() => playlist_items.playlist_item_id),
 	value: int().notNull(),
 	mods: text(),
+	player: int()
+		.notNull()
+		.references(() => players.osu_id),
 	created: int({ mode: "timestamp" }).notNull(),
 });
 
-export const users = sqliteTable("users", {
-	user_id: int().notNull().primaryKey(),
+export const players = sqliteTable("players", {
+	osu_id: int().notNull().primaryKey(),
 	name: text().notNull(),
 });
